475085d82cb025f0416074be523df4d517cb8e33
Ahmad Kamal <ahmadalikamal11@gmail.com>
2025-10-21 15:11:10 +0300

    Initial project upload

---
+++ PrScript.js
@@ -0,0 +1,11 @@
+const warpper = document.querySelector('.warpper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click',()=>{
+    warpper.classList.add('active');
+});
+
+loginLink.addEventListener('click',()=>{
+    warpper.classList.remove('active');
+});
