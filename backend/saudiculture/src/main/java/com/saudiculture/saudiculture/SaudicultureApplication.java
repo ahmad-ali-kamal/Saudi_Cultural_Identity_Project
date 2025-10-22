87382d86451fce13f5ad622a639980d9f9002c96
M-Alhaig <mohammedalhaij100@gmail.com>
2025-10-22 21:02:54 +0300

    Initialize backend project for "saudiculture" using Spring Boot with Maven configuration. Includes basic project structure, dependencies, and initial setup.

---
+++ backend/saudiculture/src/main/java/com/saudiculture/saudiculture/SaudicultureApplication.java
@@ -0,0 +1,13 @@
+package com.saudiculture.saudiculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudicultureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaudicultureApplication.class, args);
+	}
+
+}
---
+++ backend/saudiculture/src/test/java/com/saudiculture/saudiculture/SaudicultureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saudiculture.saudiculture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudicultureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
