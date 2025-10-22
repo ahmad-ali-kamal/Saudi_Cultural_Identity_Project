e681e29bd8c065ffd22181d23fef72e65367c764
M-Alhaig <mohammedalhaij100@gmail.com>
2025-10-22 22:21:13 +0300

    Add Question model, repository, and MongoDB validation dependency to backend

---
+++ backend/src/main/java/com/saudiculture/repositories/QuestionRepository.java
@@ -0,0 +1,8 @@
+package com.saudiculture.repositories;
+
+import com.saudiculture.models.Question;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface QuestionRepository extends MongoRepository<Question, Long> {
+
+}
