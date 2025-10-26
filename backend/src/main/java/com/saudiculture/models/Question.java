afe403b195d12b0034fa514dcadcbbae2f05cc0f
M-Alhaig <mohammedalhaij100@gmail.com>
2025-10-26 21:32:00 +0300

    Add compound indexes to `Question` model for improved query performance

--- backend/src/main/java/com/saudiculture/models/Question.java
+++ backend/src/main/java/com/saudiculture/models/Question.java
@@ -3,11 +3,15 @@ package com.saudiculture.models;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
 @Document(collection = "questions")
+@CompoundIndex(name = "category_region_index", def = "{'category': 1, 'region': 1}")
+@CompoundIndex(name = "category_type_index", def = "{'category': 1, 'type': 1}")
+@CompoundIndex(name = "region_type_index", def = "{'region': 1, 'type': 1}")
 public class Question {
 
   @Id
@@ -38,6 +42,4 @@ public class Question {
 
   @NotBlank(message = "Region cannot be blank")
   private String region;
-
-
 }
